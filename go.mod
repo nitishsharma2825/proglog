41a68814b925711bc66a8d2870d4791489774c4a
nitishsharma2825 <sharnit2825@gmail.com>
2025-04-13 23:07:31 +0530

    ch8: Adding consensus using raft library

--- go.mod
+++ go.mod
@@ -5,6 +5,8 @@ go 1.23.2
 require (
 	github.com/casbin/casbin v1.9.1
 	github.com/grpc-ecosystem/go-grpc-middleware v1.4.0
+	github.com/hashicorp/raft v1.7.3
+	github.com/hashicorp/raft-boltdb v0.0.0-20250225060035-8f7048cdfa53
 	github.com/hashicorp/serf v0.10.2
 	github.com/stretchr/testify v1.10.0
 	github.com/travisjeffery/go-dynaport v1.0.0
@@ -20,20 +22,26 @@ require (
 	github.com/Knetic/govaluate v3.0.1-0.20171022003610-9aa49832a739+incompatible // indirect
 	github.com/armon/go-metrics v0.4.1 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/fatih/color v1.13.0 // indirect
 	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/google/btree v1.1.2 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-hclog v1.6.2 // indirect
 	github.com/hashicorp/go-immutable-radix v1.3.1 // indirect
 	github.com/hashicorp/go-metrics v0.5.4 // indirect
+	github.com/hashicorp/go-msgpack v0.5.5 // indirect
 	github.com/hashicorp/go-msgpack/v2 v2.1.2 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/hashicorp/go-sockaddr v1.0.5 // indirect
 	github.com/hashicorp/golang-lru v1.0.2 // indirect
 	github.com/hashicorp/memberlist v0.5.2 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/miekg/dns v1.1.56 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/sean-/seed v0.0.0-20170313163322-e2103e2c3529 // indirect
+	go.etcd.io/bbolt v1.3.5 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
 	golang.org/x/mod v0.17.0 // indirect
 	golang.org/x/net v0.34.0 // indirect
@@ -44,3 +52,5 @@ require (
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20250115164207-1a7da9e5054f // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
+
+replace github.com/hashicorp/raft-boltdb => github.com/travisjeffery/raft-boltdb v1.0.0
---
+++ internal/log/distributed.go
@@ -0,0 +1,137 @@
+package log
+
+import (
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/hashicorp/raft"
+	raftboltdb "github.com/hashicorp/raft-boltdb"
+)
+
+type DistributedLog struct {
+	config Config
+	log    *Log
+	raft   *raft.Raft
+}
+
+func NewDistributedLog(dataDir string, config Config) (*DistributedLog, error) {
+	l := &DistributedLog{
+		config: config,
+	}
+	if err := l.setupLog(dataDir); err != nil {
+		return nil, err
+	}
+	if err := l.setupRaft(dataDir); err != nil {
+		return nil, err
+	}
+	return l, nil
+}
+
+func (l *DistributedLog) setupLog(dataDir string) error {
+	logDir := filepath.Join(dataDir, "log")
+	if err := os.MkdirAll(logDir, 0755); err != nil {
+		return err
+	}
+	var err error
+	l.log, err = NewLog(logDir, l.config)
+	return err
+}
+
+/*
+Raft Instances comprises:
+- A FSM that applied commands you give raft
+- log store where raft stores these commands
+- A stable store where raft stores cluster configs [servers in cluster, addresses]
+- A snapshot store where raft stores compact snapshots of its data
+- A transport that raft uses to connect with server's peers
+*/
+func (l *DistributedLog) setupRaft(dataDir string) error {
+	fsm := &fsm{
+		log: l.log,
+	}
+
+	logDir := filepath.Join(dataDir, "raft", "log")
+	if err := os.MkdirAll(logDir, 0755); err != nil {
+		return err
+	}
+	logConfig := l.config
+	logConfig.Segment.InitialOffset = 1
+	logStore, err := newLogStore(logDir, logConfig)
+	if err != nil {
+		return err
+	}
+
+	stableStore, err := raftboltdb.NewBoltStore(
+		filepath.Join(dataDir, "raft", "stable"),
+	)
+	if err != nil {
+		return err
+	}
+	retain := 1
+
+	snapshotStore, err := raft.NewFileSnapshotStore(
+		filepath.Join(dataDir, "raft"),
+		retain,
+		os.Stderr,
+	)
+	if err != nil {
+		return err
+	}
+
+	maxPool := 5
+	timeout := 10 * time.Second
+	transport := raft.NewNetworkTransport(
+		l.config.Raft.StreamLayer,
+		maxPool,
+		timeout,
+		os.Stderr,
+	)
+
+	config := raft.DefaultConfig()
+	config.LocalID = l.config.Raft.LocalID
+	if l.config.Raft.HeartbeatTimeout != 0 {
+		config.HeartbeatTimeout = l.config.Raft.HeartbeatTimeout
+	}
+	if l.config.Raft.ElectionTimeout != 0 {
+		config.ElectionTimeout = l.config.Raft.ElectionTimeout
+	}
+	if l.config.LeaderLeaseTimeout != 0 {
+		config.LeaderLeaseTimeout = l.config.Raft.LeaderLeaseTimeout
+	}
+	if l.config.Raft.CommitTimeout != 0 {
+		config.CommitTimeout = l.config.Raft.CommitTimeout
+	}
+
+	l.raft, err = raft.NewRaft(
+		config,
+		fsm,
+		logStore,
+		stableStore,
+		snapshotStore,
+		transport,
+	)
+	if err != nil {
+		return err
+	}
+	hasState, err := raft.HasExistingState(
+		logStore,
+		stableStore,
+		snapshotStore,
+	)
+	if err != nil {
+		return err
+	}
+	if l.config.Raft.Bootstrap && !hasState {
+		config := raft.Configuration{
+			Servers: []raft.Server{
+				{
+					ID:      config.LocalID,
+					Address: transport.LocalAddr(),
+				},
+			},
+		}
+		err = l.raft.BootstrapCluster(config).Error()
+	}
+	return err
+}
